1e3d88ffb36287f62cfc453540d29f42477b6e0c
DanielCarlos <daniel.carlos.souza@gmail.com>
2019-09-08 10:57:24 -0300

    Implementacao alert dialog

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'models/items.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage() {
+    items = [];
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var newTaskController = TextEditingController();
+  var _textFieldController = TextEditingController();
+
+  _HomePageState() {
+    load();
+  }
+  void add() {
+    if (_textFieldController.text.isEmpty) return;
+    setState(() {
+      widget.items.add(
+        Item(
+            id: new DateTime.now().millisecondsSinceEpoch,
+            title: _textFieldController.text,
+            done: false),
+      );
+      _textFieldController.clear();
+      Navigator.of(context).pop();
+      save();
+    });
+  }
+
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+      save();
+    });
+  }
+
+  Future load() async {
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if (data != null) {
+      Iterable decoded = jsonDecode(data);
+
+      List<Item> result = decoded.map((x) => Item.fromJson(x)).toList();
+
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('data', jsonEncode(widget.items));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text("Lista de Tarefas"),
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          final item = widget.items[index];
+          return Dismissible(
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                });
+                save();
+              },
+            ),
+            key: Key(item.id.toString()),
+            background: Container(
+              color: Colors.red.withOpacity(0.5),
+              child: Icon(Icons.remove_circle),
+            ),
+            onDismissed: (direction) {
+              remove(index);
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (_) => AlertDialog(
+                    title: Text('Adicionar Tarefa'),
+                    content: TextField(
+                      controller: _textFieldController,
+                      decoration: InputDecoration(hintText: "Tarefa"),
+                    ),
+                    actions: <Widget>[
+                      new FlatButton(
+                        child: new Text('Adicionar'),
+                        onPressed: add,
+                      )
+                    ],
+                  ));
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
